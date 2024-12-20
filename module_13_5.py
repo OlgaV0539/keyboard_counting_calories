ba7b51eca44d6966b0a86eefd6bc34da67ae578d
OlgaV0539 <olv-personal@mail.ru>
2024-12-20 14:38:58 +0700

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,63 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+api = "7685742774:AAGT0px4BEPsER9DkfnxEWzDjhaS9_qI1AE"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button1, button2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message: types.Message):
+    await message.answer("Введите свой возраст(г):")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    age = message.text
+    await state.update_data(age=age)
+    await message.answer("Введите свой рост(см):")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    growth = message.text
+    await state.update_data(growth=growth)
+    await message.answer("Введите свой вес(кг):")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def finish(message: types.Message, state: FSMContext):
+    weight = message.text
+    await state.update_data(weight=weight)
+    data = await state.get_data()
+    kcal = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) - 161
+    await message.answer(f"Женщине Вашего возраста, веса и роста необходимо потреблять "
+                         f"{kcal} ккал в сутки")
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
